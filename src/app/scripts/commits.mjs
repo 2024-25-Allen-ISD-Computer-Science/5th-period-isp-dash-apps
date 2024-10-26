521b9ba023676c7e9d438014f3ba3d6270559a47
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2024-10-26 23:35:18 +0000

    created folder for scripts and finished writing Github Api commit data script to fetch all the commit data from a repo and return it in json format. Also made a components folder for storing component code

---
+++ src/app/scripts/commits.mjs
@@ -0,0 +1,44 @@
+import {Octokit} from "octokit";
+//fetch and jsonify all the commits in a timespan for a single repo i.e for the page of a single application
+async function repo_fetch(key,repo,owner){
+  commits = {}
+  const octokit = new Octokit({auth:key})
+  var res = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+    owner: owner,
+    repo: repo,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+      }
+  })
+  for (var i =0;i < res.data.length;i++){
+    const commit = res.data[i]
+    var author = commit["author"]
+    
+    //this object contains two properties for user info and commit info called user_info and commit_info respectively
+    var commit_json = {
+                        user_info:
+                        {
+                          author:author["login"],
+                          profile_link:author["html_url"],
+                          user_avatar:author["avatar_url"],
+                          user_id:author["id"],
+                          type:author["type"]
+                        },
+                        commit_info:
+                        {
+                          sha:commit["sha"],
+                          url:commit["html_url"],
+                          message:commit["commit"]["message"]
+                        }
+                      }
+                      Object.defineProperty(commits,i, commit_json);
+  }
+  return commits
+}
+
+//fetch and jsonify all the commits in a timespan across a whole organization
+function org_fetch(){}
+console.log(await repo_fetch("placeholder","https://example.org/products.json"));
+//commit_fetch.exports = {repo_fetch,org_fetch}
+// Octokit.js
+// https://github.com/octokit/core.js#readme
