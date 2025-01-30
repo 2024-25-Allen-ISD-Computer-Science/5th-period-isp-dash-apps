cc15df3a40c0ca23f3dcb9b49ab25af90ddd7ebd
Braylond73 <142352244+Braylond73@users.noreply.github.com>
2025-01-30 15:58:16 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,14 @@ export default function Home() {
     <div className={`${RubikRegular.className} container text-20px`}>The Dash Software and Web Application Suite is a collection of open source applications with the aim of
        making free and convienient software options available for everyone to use and contribute to.
       We have a dedicated team working to develop these apps and are open to suggestions for new ideas on projects we can create </div>
+    
+    <div className={`${RubikBold.className} container text-40px`}> FOUNDERS/CONTRIBUTERS</div>
+
+    <div className={`${RubikRegular.className} container text-20px`}> 
+     Logan Is the product manger and producer on the team 
+     Rithvik is the programmer And follows in logan footsteps and Debugger
+     Braylon is the researcher and Edior in dash apps helps Find ideas on projects and can adapt.
+</div>
     </>
   );
 }
