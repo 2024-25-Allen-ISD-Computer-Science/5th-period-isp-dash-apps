8c5b682332eea8e46710652515f75fda7d9567a0
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2025-02-06 09:28:54 -0600

    added template map function

--- src/app/components/commit.tsx
+++ src/app/components/commit.tsx
@@ -1,7 +1,9 @@
 export default function Commit({commit_info}) {
     return (
-      <>
-    
-      </>
+        commit_info.map(map_element)
     );
   }
+
+function map_element(){
+    return "hello";
+}
